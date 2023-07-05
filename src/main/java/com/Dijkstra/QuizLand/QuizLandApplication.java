386de6ab6ef8a713e4b825c34b72257f271336f9
Dijkstra <besztak.daniel@gmail.com>
2023-07-05 18:59:24 +0200

    Initial commit

---
+++ src/main/java/com/Dijkstra/QuizLand/QuizLandApplication.java
@@ -0,0 +1,13 @@
+package com.Dijkstra.QuizLand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizLandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dijkstra/QuizLand/QuizLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dijkstra.QuizLand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
