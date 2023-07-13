29d5296404828bad81792405b072b4bb60d2b649
Dijkstra <besztak.daniel@gmail.com>
2023-07-13 18:18:05 +0200

    [FEAT] Edit DTO instanfe responsible for read questions.

--- src/main/java/com/Dijkstra/QuizLand/Question/QuestionReadDTO.java
+++ src/main/java/com/Dijkstra/QuizLand/Question/QuestionReadDTO.java
@@ -3,21 +3,21 @@ package com.Dijkstra.QuizLand.Question;
 import com.Dijkstra.QuizLand.Question.Enum.Category;
 import com.Dijkstra.QuizLand.Question.Enum.Difficulty;
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
-
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Getter
 public class QuestionReadDTO{
+    private final int id;
     private final String questionContent;
     private final Set<String> options;
     private final List<Category> category;
     private final Difficulty difficulty;
     private final boolean active;
 
-    QuestionReadDTO(@NotNull Question questionSource){
+    QuestionReadDTO(Question questionSource){
+        this.id = questionSource.getId();
         this.questionContent = questionSource.getQuestionContent();
         this.options = questionSource
                 .getOptions()
